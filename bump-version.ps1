49822d54c284a4816dfb39f27f95e442f54dfc32
Adam Kawik <akawik@gmail.com>
2026-03-19 13:03:24 +0100

    Add version bump script with git hooksbinding fix

--- bump-version.ps1
+++ bump-version.ps1
@@ -7,4 +7,6 @@ $parts[2] = [int]$parts[2] + 1
 $newVersion = "$($parts[0]).$($parts[1]).$($parts[2])"
 $newContent = $content -replace "<Version>$currentVersion</Version>", "<Version>$newVersion</Version>"
 Set-Content $csproj $newContent
+# Stage the modified csproj file for commit
+& git add $csproj
 Write-Host "Nowa wersja: $newVersion"
